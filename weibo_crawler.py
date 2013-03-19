bb8840de18522d7f015bb5a3dd67e4eae485d996
Tony.Shao <xiocode@gmail.com>
2013-03-19 10:31:09 +0800

    update

--- weibo_crawler.py
+++ weibo_crawler.py
@@ -16,12 +16,11 @@ from xtimeline.actions.friendships import start as friendship_creator
 
 
 if __name__ == '__main__':
-    # crawler_process = Process(target=status_crawler)
-    # creator_process = Process(target=friendship_creator)
+    crawler_process = Process(target=status_crawler)
+    creator_process = Process(target=friendship_creator)
     # #poster_process = Process(target=status_poster)
-    # crawler_process.start()
-    # creator_process.start()
+    crawler_process.start()
+    creator_process.start()
     #poster_process.start()
-    friendship_creator()
 
 
