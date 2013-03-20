fd194e2c26ab39cb52e026865ce3fce96a15f9b2
Tony.Shao <xiocode@gmail.com>
2013-03-20 14:04:00 +0800

    update

--- xtimeline/algorithms/top.py
+++ xtimeline/algorithms/top.py
@@ -7,4 +7,4 @@ from math import log
 def scores(reposts_count, comments_count, counter=1):
     if comments_count > reposts_count:
         comments_count = (comments_count - reposts_count) * 0.001
-    return log(reposts_count * 0.2 + comments_count * 5 + counter * 100)
+    return log(reposts_count * 0.2 + comments_count * 5 + counter * 200)
